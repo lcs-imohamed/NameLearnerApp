7a98b5e6d66334c2fe1011803047a2c63c1a36a9
Ilana Mohamed <Imohamed@lcs.on.ca>
2021-04-27 19:03:39 -0400

    Initial Commit

---
+++ NameLearnerApp/NameLearnerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameLearnerAppApp.swift
+//  NameLearnerApp
+//
+//  Created by Mohamed, Ilana on 2021-04-27.
+//
+
+import SwiftUI
+
+@main
+struct NameLearnerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
